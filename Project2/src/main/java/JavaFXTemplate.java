97e7e6a0bb4f7dc7bc3d172a54a33b2f3d36b862
J <jchen4313@gmail.com>
2020-10-15 01:12:11 -0500

    Color Menu/Return/Exit

--- Project2/src/main/java/JavaFXTemplate.java
+++ Project2/src/main/java/JavaFXTemplate.java
@@ -21,11 +21,14 @@ import javafx.scene.text.Font;
 import javafx.util.Duration;
 
 import java.util.HashMap;
+import java.util.Random;
+
+import static javafx.application.Platform.exit;
 
 
 public class JavaFXTemplate extends Application {
 
-		private Button b1;
+		private Button returnButton;
 		
 		private TextField t1;
 		
@@ -34,8 +37,6 @@ public class JavaFXTemplate extends Application {
 		private Text txt;
 
 		private EventHandler<ActionEvent> myHandler;
-
-		private HashMap<String, Scene> sceneMap;
 
 		private PauseTransition pause = new PauseTransition(Duration.seconds(3));
 
@@ -48,30 +49,80 @@ public class JavaFXTemplate extends Application {
 	}
 
 	//method to create our first scene with controls
-//	public Scene createControlScene() {
+	public Scene createGameScene() {
+		BorderPane pane = new BorderPane();
+		pane.setStyle("-fx-background-color: maroon;");
+
+		GridPane grid = new GridPane();
+		GridPane spotGrid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		spotGrid.setAlignment((Pos.CENTER));
+
+		//Creating the vertical box
+		VBox verticalB = new VBox(25,menu,spotGrid);
+		verticalB.getChildren().addAll(grid);
+		toggleGrid(grid, true);
+
+		pane.setCenter(verticalB);
+
+		//Adds the buttons into the grid
+		addBetCard(grid);
+		addSpots(spotGrid);
+
+		//new scene with root node
+		return new Scene(pane, 1000,700);
+	}
+
+	public Scene createRulesScene(){
+		BorderPane rulesPane = new BorderPane();
+
+		rulesPane.setStyle("-fx-background-color: maroon;");
+		rulesPane.setBottom(returnButton);
+
+		String kenoRules = "Rules of Keno Gambling Game: \n" +
+				"Players wager by choosing a set amount of numbers" +
+				"\n (pick 2 numbers, pick 10 numbers, etc.)" +
+				" ranging from 1 to 80.\n After all players have made their wagers" +
+				" and picked their numbers,\n twenty numbers are drawn at random," +
+				" between 1 and 80 with no\n duplicates." +
+				" Players win by matching a set amount of their\n numbers to" +
+				" the numbers that are randomly drawn.\n";
+
+		txt.setFont(Font.font(20));
+		txt.setText(kenoRules);
+		txt.setTextAlignment(TextAlignment.CENTER);
+
+		VBox verticalBox = new VBox(25,menu, txt);
+		rulesPane.setCenter(verticalBox);
+
+		return new Scene(rulesPane, 1000,700);
+	}
+
+//	public Scene createWinningsMenu(){
+//		BorderPane rulesPane = new BorderPane();
+//		rulesPane.setStyle("-fx-background-color: maroon;");
 //
-//		BorderPane pane = new BorderPane();
-//		pane.setPadding(new Insets(70));
-//
-//		VBox paneCenter = new VBox(10, text, addToListBtn, printListBtn, displayQueueItems);
-//
-//		pane.setCenter(paneCenter);
-//		pane.setLeft(sceneChangeBtn);
-//		pane.setStyle("-fx-background-color: lightPink;");
-//
-//
-//		return new Scene(pane, 850, 750);
+//		return new Scene();
 //	}
+
+//	public Scene createNewLook(){
+//		BorderPane looksPane = new BorderPane();
+//		String stringArr[] = {"maroon;", "lightblue;", "black;", "white;"};
+//
+//		Random rand = null;
+//		looksPane.setStyle("-fx-background-color: "+ stringArr[rand.nextInt(3)]);
+//
+//
+//		return new Scene(looksPane, 1000, 700);
+//	}
+
+
 
 	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		sceneMap = new HashMap<String,Scene>();
 		primaryStage.setTitle("Welcome to Keno");
-
-		
-		b1 = new Button();
-		t1 = new TextField();
+		returnButton = new Button("Return to Game");
 		txt = new Text();
 		
 		/*//create an event handler if more than one widget needs same action
@@ -96,25 +147,33 @@ public class JavaFXTemplate extends Application {
 		
 		//use a lambda expression to attach the event handler to a button
 //		b1.setOnAction(e->t1.setText("I love this syntax!!!!"));
-
+		/** Menu Option Code **/
+		/*********************************************************************************************************/
+		//Creates all the menu and the drop down items
 		menu = new MenuBar(); //a menu bar takes menus as children
 		Menu optionMenu = new Menu("Menu");
 		MenuItem rulesMenu = new MenuItem("Rules of the Game"); //a menu goes inside a menu bar
 		MenuItem winningsMenu = new MenuItem("Odds of Winning");
-
-		MenuItem iOne = new MenuItem("click me"); //menu items go inside a menu
+		MenuItem newLook = new MenuItem("New Look");
+		MenuItem exitGame = new MenuItem("Exit Game");
 		menu.getMenus().addAll(optionMenu);
 
-		//event handler for menu item
-		rulesMenu.setOnAction(rules(txt));
-
+		//Putting menu items into the menu (The drop down menu)
 		optionMenu.getItems().add(rulesMenu); //add menu item to first menu
 		optionMenu.getItems().add(winningsMenu);
+		optionMenu.getItems().add(newLook);
+		optionMenu.getItems().add(exitGame);
 
+		//
+		rulesMenu.setOnAction(e->primaryStage.setScene(createRulesScene()));
+		exitGame.setOnAction((e->exit()));
 
-		// This code demonstrates how to use a GridPane. Might be useful for your project
+//		winningsMenu.setOnAction(e->primaryStage.setScene();
+		primaryStage.show();
+		/*********************************************************************************************************/
 
-		// event handler is attached to each button in the GridPane
+		/** Temporary EVENTHANDLER **/
+		/*********************************************************************************************************/
 		myHandler = new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e) {
 				System.out.println("button pressed: " + ((Button)e.getSource()).getText());
@@ -123,25 +182,10 @@ public class JavaFXTemplate extends Application {
 			}
 		};
 
-		GridPane grid = new GridPane();
-		GridPane spotGrid = new GridPane();
-		grid.setAlignment(Pos.CENTER);
-		addBetCard(grid); //populate the GridPane with buttons
+		/*********************************************************************************************************/
 
-		spotGrid.setAlignment((Pos.CENTER));
-		addSpots(spotGrid);
-
-
-		//Creating the vertical box
-
-		VBox verticalB = new VBox(25,menu,spotGrid);
-		verticalB.getChildren().addAll(grid,txt);
-		toggleGrid(grid, true);
-
-		//new scene with root node
-		Scene sceneM = new Scene(verticalB, 1000,700);
-
-		primaryStage.setScene(sceneM); //set the scene in the stage
+		returnButton.setOnAction(e->primaryStage.setScene(createGameScene()));
+		primaryStage.setScene(createGameScene()); //set the scene in the stage
 		primaryStage.show(); //make visible to the user
 	}
 
@@ -168,6 +212,7 @@ public class JavaFXTemplate extends Application {
 			}
 		}
 	}
+
 	/*
 	 * method to populate a GridPane with buttons and attach a handler to each button
 	 * Spots
@@ -184,29 +229,5 @@ public class JavaFXTemplate extends Application {
 			spots.setOnAction(myHandler);
 			grid.add(spots,i,0);
 		}
-	}
-
-
-	public EventHandler<ActionEvent> rules(Text txt)
-	{
-		String kenoRules = "Rules of Keno Gambling Game: \n" +
-		"Players wager by choosing a set amount of numbers" +
-				"\n (pick 2 numbers, pick 10 numbers, etc.)" +
-		" ranging from 1 to 80.\n After all players have made their wagers" +
-		" and picked their numbers,\n twenty numbers are drawn at random," +
-			" between 1 and 80 with no\n duplicates." +
-			" Players win by matching a set amount of their\n numbers to" +
-			" the numbers that are randomly drawn.\n";
-
-		txt.setFont(Font.font(20));
-		txt.setText(kenoRules);
-		txt.setTextAlignment(TextAlignment.CENTER);
-
-		return null;
-	}
-
-	public void odds(Text txt)
-	{
-
 	}
 }
